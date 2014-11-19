3c5630fccc134d15c89cb608ff184f48c4486939
vampaynani <vampay@digitaldealers.mx>
2014-11-18 18:25:52 -0600

    vampay changes

--- nivelesAltos/www/js/app.js
+++ nivelesAltos/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'angles', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -45,6 +45,7 @@ angular.module('starter', ['ionic', 'starter.controllers'])
       views: {
         'menuContent' :{
           templateUrl: "templates/nse.html",
+          controller: "NSECtrl"
         }
       }
     })
--- nivelesAltos/www/js/controllers.js
+++ nivelesAltos/www/js/controllers.js
@@ -10,9 +10,135 @@ angular.module('starter.controllers', [])
 		*/
 	}
 })
+.controller('NSECtrl', function($scope, $ionicNavBarDelegate, $ionicPopover){
+	$ionicPopover.fromTemplateUrl('popoverAB', {
+		scope: $scope,
+	}).then(function(popover){
+		$scope.popoverAB = popover;
+	});
+	$ionicPopover.fromTemplateUrl('popoverC', {
+		scope: $scope,
+	}).then(function(popover){
+		$scope.popoverC = popover;
+	});
+	$ionicPopover.fromTemplateUrl('popoverCMin', {
+		scope: $scope,
+	}).then(function(popover){
+		$scope.popoverCMin = popover;
+	});
+	$scope.openPopover = function($event, popover){
+		$scope[popover].show($event);
+	}
+	$scope.closePopover = function(popover){
+		$scope[popover].hide();
+	}
+	$scope.$on('$destroy', function(){
+		$scope.popoverAB.remove();
+		$scope.popoverC.remove();
+		$scope.popoverCMin.remove();
+	});
+})
 .controller('CaracteristicasBaseCtrl', function($scope, $ionicNavBarDelegate) {
 	$scope.goBack = function(){
 		$ionicNavBarDelegate.back();
+	}
+	$scope.options = {
+		animation: true,
+		animationEasing: "easeOutQuart",
+		showScale: true,
+		responsive: true
+	}
+	$scope.chart0 = [
+		{
+			value: 60,
+			label: "Estudió en el extranjero",
+			color: "#da4418",
+			highlight: "#ee3f0c"
+		},
+		{
+			value: 40,
+			label: "Estudió en México",
+			color: "#e7c500",
+			highlight: "#ffd900"
+		}
+	]
+	$scope.chart2 = [
+		{
+			value: 23,
+			label: "No tiene hijos",
+			color: "#00c6d9",
+			highlight: "#00e9ff"
+		},
+		{
+			value: 77,
+			label: "Tiene hijos",
+			color: "#e7c500",
+			highlight: "#ffd900"
+		}
+	]
+	$scope.chart3 = [
+		{
+			value: 93,
+			label: "Tiene casa propia",
+			color: "#e7c500",
+			highlight: "#ffd900"
+		},
+		{
+			value: 7,
+			label: "No tiene casa propia",
+			color: "#da4418",
+			highlight: "#ee3f0c"
+		}
+	]
+	$scope.chart4 = [
+		{
+			value: 3,
+			label: "No",
+			color: "#00c6d9",
+			highlight: "#00e9ff"
+		},
+		{
+			value: 97,
+			label: "Si",
+			color: "#e7c500",
+			highlight: "#ffd900"
+		}
+	];
+	$scope.chart5 = [
+		{
+			value: 17,
+			label: "No",
+			color: "#00c6d9",
+			highlight: "#00e9ff"
+		},
+		{
+			value: 83,
+			label: "Si",
+			color: "#e7c500",
+			highlight: "#ffd900"
+		}
+	];
+	$scope.chart6 = {
+		labels: ["Casa","Móvil","Trabajo","Internet Móvil","Wi-fi Gratis","Escuela"],
+		datasets: [
+			{
+				fillColor: "#e7c500",
+				strokeColor: "#999",
+				highlightFill: "#ffd900",
+				data:[92,86,77,55,21,6]
+			}
+		]
+	}
+	$scope.chart7 = {
+		labels: ["Tarjeta","Plan","Plan/Tarjeta"],
+		datasets: [
+			{
+				fillColor: "#e7c500",
+				strokeColor: "#999",
+				highlightFill: "#ffd900",
+				data:[13,82,6]
+			}
+		]
 	}
 })
 .controller('HistoriasVidaCtrl', function($scope, $ionicNavBarDelegate) {
